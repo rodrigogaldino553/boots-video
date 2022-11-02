534794bdb5d8000763becceccc2f4f3a15a23438
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2022-11-02 11:16:00 -0300

    add name, avatar fields to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one_attached :avatar
+
   def username
     self[:email].split('@').first
   end
+
 end
--- db/migrate/20220930025016_add_custom_fields_to_user.rb
+++ db/migrate/20220930025016_add_custom_fields_to_user.rb
@@ -1,5 +1,5 @@
 class AddCustomFieldsToUser < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :name, :string
+    add_column :users, :name, :string, default: 'no name'
   end
 end
