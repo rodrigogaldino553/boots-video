ae1cd39cc9d24f22409941829083c2fbc610a1fe
rodrigogaldino553 <galdinorodrigo553@gmail.com>
2022-11-02 14:38:20 -0300

    add video preview card

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,6 @@
 class Video < ApplicationRecord
   has_one_attached :content
-  has_one_attached :video_thumbnail
+  has_one_attached :thumbnail
 
   validates :title, presence: true, length: {maximum: 20}, length: {minimum: 1}
   validates :description, presence: true, length: {maximum: 200}, length: {minimum: 1} #rich text
@@ -8,7 +8,7 @@ class Video < ApplicationRecord
     content_type: ['video/mp4'],
     size: { less_than: 50.megabytes, message: 'size shuld be under 50MB!' }
 
-  validates :video_thumbnail, presence: true,
+  validates :thumbnail, presence: true,
     content_type: ['image/png', 'image/jpg', 'image/jpeg'],
     size: { less_than: 2.megabytes, message: 'size shuld be under 2MB!' }
   
